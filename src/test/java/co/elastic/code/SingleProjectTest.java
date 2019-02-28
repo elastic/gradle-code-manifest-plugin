72bad0b37418f6126adc09f959800194042dd092
Poytr1 <pengcheng.xu@elastic.co>
2019-02-28 12:27:39 +0800

    Compare file generated with original config file

--- src/test/java/co/elastic/code/SingleProjectTest.java
+++ src/test/java/co/elastic/code/SingleProjectTest.java
@@ -7,34 +7,4 @@ public class SingleProjectTest extends BaseProjectTest {
         return "singleProject";
     }
 
-    @Override
-    protected String getExpectedConfig() {
-        return "{\n" +
-                "  \"projectInfos\": [\n" +
-                "    {\n" +
-                "      \"android\": false,\n" +
-                "      \"path\": \":\",\n" +
-                "      \"repos\": [\n" +
-                "        {\n" +
-                "          \"repoType\": \"MAVEN\",\n" +
-                "          \"url\": \"https://repo.maven.apache.org/maven2/\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"dependencies\": [\n" +
-                "        {\n" +
-                "          \"groupId\": \"com.google.guava\",\n" +
-                "          \"artifactId\": \"guava\",\n" +
-                "          \"version\": \"18.0\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"srcDirs\": [\n" +
-                "        \"src/main/java\"\n" +
-                "      ],\n" +
-                "      \"testSrcDirs\": [\n" +
-                "        \"src/test/java\"\n" +
-                "      ]\n" +
-                "    }\n" +
-                "  ]\n" +
-                "}";
-    }
 }
